b803e8818a4466e494e200b624fd65843712afc6
Balogun Taoheed <baloguntaoheed2@gmail.com>
2021-10-12 18:21:02 -0700

    just a simple game

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GameScreen from "./Component/GameScreen";
+
+const App = () => {
+    return (
+        <div>
+            <GameScreen/>
+        </div>
+    )
+}
+
+export default App
